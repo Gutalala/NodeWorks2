22a9c5b1b3001d2b07e9d0ca97b5b54fd5a1bbc8
Agent W <bakury19004@gmail.com>
2018-07-11 02:58:32 -0600

    probably not that important, ran from shell

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
-const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+express()
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
