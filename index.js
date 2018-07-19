8a043ecf75fd736cf201c942cb601267c8719866
Agent W <bakury19004@gmail.com>
2018-07-19 07:25:56 -0600

    probably not that important, ran from shell

--- index.js
+++ index.js
@@ -36,9 +36,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', handleLoginRequest){
-  	console.log("connection successful");
-  }
+app.get('/', handleLoginRequest)
 
 
 
