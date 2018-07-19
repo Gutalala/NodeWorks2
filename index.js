0b449c1bff26536d67daf027c0a9c32a3fdd1923
Agent W <bakury19004@gmail.com>
2018-07-18 20:26:37 -0600

    probably not that important, ran from shell

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var path = require('path');
-//var bodyParser = require('bodyParser'); 
+var bodyParser = require('bodyParser'); 
 const PORT = process.env.PORT || 5000;
 
 var app = express()
   //.set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('index'))
   //.get('/cool', (req, res) => res.send(cool()))
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var logger = function(req, res){
+	console.log('I\'m really really good');
+}
+
+app.use(logger);
+
+  app.get()
+
   app.listen(PORT, function() {
   	console.log(`Listening on ${ PORT }`);
   }) 
