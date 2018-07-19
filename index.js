e9b40f0d1116befe4abdef59fae3b5aae5a6e1ee
Agent W <bakury19004@gmail.com>
2018-07-19 09:50:12 -0600

    probably not that important, ran from shell

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser'); 
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 
 //Connect to Postgres
-var connectionString = process.env.DATABASE_URL;
+var connectionString = process.env.DATABASE_URL || "postgres://fox19:Cunmiu1904@localhost:5432/NodeWorks";
 const pool = new Pool({connectionString: connectionString});
 
 function handleLoginRequest(req, res){
@@ -34,12 +36,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get('/', handleLoginRequest)
-
-
-
-  app.listen(PORT, function() {
+app.listen(PORT, function() {
   	console.log(`Listening on ${ PORT }`);
   }) 
+
+//Home Page
+app.get('/', function(req, res){
+	res.render('index');
+})
+
+//Render Login Page
+app.get('/login', function(req, res){
+	res.render('login');
+})
+
+//Render Register Page
+app.get('/register', function(req, res){
+	res.render('register');
+})
+
+//Redirect to Login Page
+app.post('/login', function(req, res){
+	return res.redirect('/login');
+})
+
+//Redirect to Register Page
+app.post('/register', function(req, res){
+	return res.redirect('/register');
+})
+
+
+
+  
