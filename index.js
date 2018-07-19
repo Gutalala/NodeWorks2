2ba70560884ad97a18c1d23b21c42bca85e9ab8d
Agent W <bakury19004@gmail.com>
2018-07-19 07:21:45 -0600

    probably not that important, ran from shell

--- index.js
+++ index.js
@@ -2,29 +2,46 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser'); 
 const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+
+//Connect to Postgres
+var connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
+
+function handleLoginRequest(req, res){
+	pool.query("SELECT * FROM Users", function(err, result){
+		if (err) {
+			throw err;
+			console.log("Cant handle database request");
+		}
+		res.json(result.rows);
+	});
+}
 
 var app = express()
   //.set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('index'))
   //.get('/cool', (req, res) => res.send(cool()))
 
+//View Engines
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// var logger = function(req, res){
-// 	console.log('I\'m really really good');
-// }
-
-// app.use(logger);
-
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', handleLoginRequest){
+  	console.log("connection successful");
+  }
+
+
 
   app.listen(PORT, function() {
   	console.log(`Listening on ${ PORT }`);
   }) 
-
-  app.get('/', function(req, res){
-  	res.send("hello guys");
-  })
